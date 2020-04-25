f88ac7df67ceba00a7251fb65505b643e648d640
okhaaleksandra <64324518+okhaaleksandra@users.noreply.github.com>
2020-04-26 01:33:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle();
+            t1.Show();
+            Triangle t2 = new Triangle(3, 4, 5);
+            t2.Show();
+            Triangle t3 = new Triangle(1.5, 3, 5);
+            t3.Show();
+            Triangle t4 = new Triangle(-7, 9, 12);
+            t4.Show();
+
+            Console.WriteLine("Создано объектов:" + Triangle.count);
+            Console.ReadKey();
+        }
+    }
+}
