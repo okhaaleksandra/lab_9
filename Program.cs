b1307d3921b95dd9d1ba6c895411d3a557ff61f7
okhaaleksandra <64324518+okhaaleksandra@users.noreply.github.com>
2020-04-25 22:10:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleArray arr = new TriangleArray(3, true);
+            arr.Show();
+
+            TriangleArray.MinIndex(arr);
+
+            Console.ReadKey();
+        }
+    }
+}
